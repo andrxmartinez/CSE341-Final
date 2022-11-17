aabdfb321155a076e65933b3e83860184f904c89
Branden Torrance <btorrance86@gmail.com>
2022-11-17 17:09:40 -0500

    Add commercial route and controller

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const router = express.Router();
 router.use("/apartments", require("./apartments"));
 // router.use("/commercial", require("./commercial"));
 router.use("/houses", require("./houses"));
+router.use("/commercial", require("./commercial"));
 router.use("/land", require("./land"));
 router.use("/api-docs", require("./swagger"));
 
