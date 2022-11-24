f61156dc3bde28809487e04fb97be45efc4409bd
Jandy Kiger <76858411+jandyrae@users.noreply.github.com>
2022-11-24 07:55:19 -0800

    e2e cypress testing for apartment routes and for get one -

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,4 @@
 module.exports = {
-  // The rest of the Cypress config options go here...
-  projectId: "ecr19u",
-
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
---
+++ cypress/e2e/apartmentRoute.cy.js
@@ -0,0 +1,32 @@
+const cypressConfig = require("../../cypress_delete/cypress.config");
+
+describe("test willow API GETs", () => {
+  beforeEach(() => {
+    cy.visit("https://willowapi.onrender.com/api-docs");
+  });
+
+  it("displays apartments endpoint", () => {
+    cy.get("#operations-tag-apartments").should("have.text", "apartments");
+
+    cy.get("#operations-apartments-get_apartments_").should(
+      "have.text",
+      "GET/apartments/"
+    );
+    cy.get("#operations-apartments-post_apartments_").should(
+      "have.text",
+      "POST/apartments/"
+    );
+    cy.get("#operations-apartments-get_apartments__id_").should(
+      "have.text",
+      "GET/apartments/{id}"
+    );
+    cy.get("#operations-apartments-put_apartments__id_").should(
+      "have.text",
+      "PUT/apartments/{id}"
+    );
+    cy.get("#operations-apartments-delete_apartments__id_").should(
+      "have.text",
+      "DELETE/apartments/{id}"
+    );
+  });
+});
