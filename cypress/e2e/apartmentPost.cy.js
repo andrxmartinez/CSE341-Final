6ea12cc0513b36fe96aa0990e0638ec5c84926ed
Jandy Kiger <76858411+jandyrae@users.noreply.github.com>
2022-11-29 13:36:49 -0800

    simple PUT test completed

--- cypress/e2e/apartmentPost.cy.js
+++ cypress/e2e/apartmentPost.cy.js
@@ -5,22 +5,8 @@ describe("test willow API GETs", () => {
     cy.visit("https://willowapi.onrender.com/api-docs");
   });
 
-  it("can click on Try It Out for POST apartment listing", () => {
-    // We'll store our post json in a variable so we can reuse it...maybe
-    const postListing = `{
-        "$price":"$120,000.00",
-        "$listDate":"2012-11-10",
-        "address":"766 E 300 SALT LAKE CITY UT 84102-2263 USA",
-        "residenceType":"single family residence",
-        "yearBuilt":"2017",
-        "$sqFeet":2000,
-        "pricePerSqFeet":100.0,
-        "$availability":true,
-        "propertyDescription":"The master bedroom is large with no lack of storage, including a separate walk-in closet and additional dual closets. Feels like home the minute you pull up. The living room is warm and inviting, centered by a wood-burning fireplace and built-in shelving. A spacious breakfast area looks out to the backyard and flows into the kitchen, where you’ll find a breakfast bar, double oven and built-in cook top. ",
-        "lengthTimeListed":24
-     }`;
+  it("can click on Try It Out and POST apartment listing", () => {
     cy.get("div.scheme-container > section > label > select").select("https");
- 
     cy.get(
       "#operations-apartments-post_apartments_ > div > button"
     ).click();
