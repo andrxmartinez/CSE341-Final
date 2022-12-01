78da33bfbd564486758f1bad0d6d2e89b8e74288
Jandy Kiger <76858411+jandyrae@users.noreply.github.com>
2022-12-01 15:07:33 -0800

    remove $

--- cypress/e2e/apartmentPut.cy.js
+++ cypress/e2e/apartmentPut.cy.js
@@ -7,14 +7,14 @@ describe("test willow API PUTs", () => {
   it("can toggle HTTPS and click on Try It Out for PUT apartment listing", () => {
     // We'll store our json in a variable so we can reuse it
     const putListing = `
-        "$price":"$120,000.00",
-        "$listDate":"2012-11-10",
+        "price":"$120,000.00",
+        "listDate":"2012-11-10",
         "address":"766 E 300 SALT LAKE CITY UT 84102-2263 USA",
         "residenceType":"single family residence",
         "yearBuilt":"2017",
-        "$sqFeet":2000,
+        "sqFeet":2000,
         "pricePerSqFeet":100.0,
-        "$availability":true,
+        "availability":true,
         "propertyDescription":"The master bedroom is large with no lack of storage, including a separate walk-in closet and additional dual closets. Feels like home the minute you pull up. The living room is warm and inviting, centered by a wood-burning fireplace and built-in shelving. A spacious breakfast area looks out to the backyard and flows into the kitchen, where you’ll find a breakfast bar, double oven and built-in cook top. ",
         "lengthTimeListed":24
      `;
--- cypress/e2e/housePut.cy.js
+++ cypress/e2e/housePut.cy.js
@@ -7,14 +7,14 @@ describe("test willow API PUTs", () => {
   it("can toggle HTTPS and click on Try It Out for PUT house listing", () => {
     // We'll store our json in a variable so we can reuse it
     const putListing = `
-        "$price":"$350,000.00",
-        "$listDate":"2016-10-02",
+        "price":"$350,000.00",
+        "listDate":"2016-10-02",
         "address":"7823 W 380 JACKSON WY 92638-3233 USA",
         "residenceType":"single family residence",
         "yearBuilt":"2012",
-        "$sqFeet":1750,
+        "sqFeet":1750,
         "pricePerSqFeet":150.0,
-        "$availability":true,
+        "availability":true,
         "propertyDescription":"Truly gorgeous custom built corner lot home in the new and upcoming Sandcreek Estates! This breathtaking architecturally designed home sits on 0.28-acres with a fully landscaped front yard & a 3-car attached garage with side access. The covered front porch provides an amazing area for relaxation. Once inside the open concept living area, this space is emphasized by a custom stone gas fireplace with shiplap accent walls, cathedral ceilings & large windows overlooking the backyard. Merging into the chef-style kitchen, there is natural wood cabinetry, granite counter, a closet panty, stainless appliances & an amazing island bar. The dining area is tucked into a charming nook with a tray drop ceiling. The main floor also hosts a mudroom, a walk-in laundry, 2 great-sized bedrooms and the gorgeous master suite with a coffered ceiling, a huge walk-in closet & a spa style bathroom with his & her vanities, an amazing soaker tub & a fully tiled walk-in shower. This home comes with a full unfinished basement providing the opportunity to grow and customize. With the numerous high-end custom details in this home & the room to grow what more could you want!",
         "lengthTimeListed":31
      `;
