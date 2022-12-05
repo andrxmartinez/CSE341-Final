36543a6ddde0cc33a50de7044985be92a866ae6a
andres martinez <andresmp1103@gmail.com>
2022-12-05 13:10:02 -0700

    land testing completed

--- cypress/e2e/landGetAll.cy.js
+++ cypress/e2e/landGetAll.cy.js
@@ -5,17 +5,17 @@ describe("test willow API GETs", () => {
     cy.visit("https://willowapi.onrender.com/api-docs");
   });
 
-  it("can click on Try It Out for get all apartment listings", () => {
+  it("can click on Try It Out for get all land listings", () => {
     cy.get("div.scheme-container > section > label > select").select("https");
     cy.get(
-      "#operations-apartments-get_apartments_ > div.opblock-summary.opblock-summary-get > button"
+      "#operations-land-get_land_ > div.opblock-summary.opblock-summary-get > button"
     ).click();
     cy.get("button.btn.try-out__btn").click();
     cy.get(
-      "#operations-apartments-get_apartments_ > div.no-margin > div > div.execute-wrapper > button"
+      "#operations-land-get_land_ > div.no-margin > div > div.execute-wrapper > button"
     ).click();
     cy.get(
-      "#operations-apartments-get_apartments_ > div.no-margin > div > div.responses-wrapper > div.responses-inner > div > div > table > tbody > tr > td.response-col_status"
+      "#operations-land-get_land_ > div.no-margin > div > div.responses-wrapper > div.responses-inner > div > div > table > tbody > tr > td.response-col_status"
     ).should("have.text", 200);
     cy.log("Verify json returns all documents with a status of 200.");
   });
